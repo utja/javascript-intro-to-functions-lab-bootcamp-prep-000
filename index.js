a2e31627538b36b6eadc757303b8c86d9c4d666e
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-04-19 01:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else if (string.toLowerCase() === string) {
-    return "I can't hear you"
+    return "I can't hear you!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
